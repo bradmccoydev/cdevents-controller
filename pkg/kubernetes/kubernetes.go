62efe463c3e04e0b8caec34e1e1cea1143b533aa
Brad McCoy <bradmccoydev@gmail.com>
2023-06-06 12:49:28 +1000

    chore: add metrics
    Signed-off-by: Brad McCoy <bradmccoydev@gmail.com>

--- pkg/kubernetes/kubernetes.go
+++ pkg/kubernetes/kubernetes.go
@@ -1,58 +1,17 @@
-package provider
+package kubernetes
 
 import (
 	"context"
 	"fmt"
-	"log"
 
+	"go.uber.org/zap"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/client-go/dynamic"
 	ctrl "sigs.k8s.io/controller-runtime"
 )
 
-type Result struct {
-	metav1.TypeMeta   `json:",inline"`
-	metav1.ObjectMeta `json:"metadata,omitempty"`
-
-	Spec   ResultSpec   `json:"spec,omitempty"`
-	Status ResultStatus `json:"status,omitempty"`
-}
-
-type ResultStatus struct {
-	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
-}
-
-type ResultList struct {
-	metav1.TypeMeta `json:",inline"`
-	metav1.ListMeta `json:"metadata,omitempty"`
-	Items           []Result `json:"items"`
-}
-
-type Backend string
-
-type ResultSpec struct {
-	Backend      `json:"backend"`
-	Kind         string    `json:"kind"`
-	Name         string    `json:"name"`
-	Error        []Failure `json:"error"`
-	Details      string    `json:"details"`
-	ParentObject string    `json:"parentObject"`
-}
-
-type Failure struct {
-	Text      string      `json:"text,omitempty"`
-	Sensitive []Sensitive `json:"sensitive,omitempty"`
-}
-
-type Sensitive struct {
-	Unmasked string `json:"unmasked,omitempty"`
-	Masked   string `json:"masked,omitempty"`
-}
-
-func GetResults(name string) string {
-	ctx := context.Background()
+func GetResults(logger *zap.Logger) {
 	config := ctrl.GetConfigOrDie()
 	dynamic := dynamic.NewForConfigOrDie(config)
 	namespace := "k8sgpt-operator-system"
@@ -60,80 +19,37 @@ func GetResults(name string) string {
 	resourceId := schema.GroupVersionResource{
 		Group:    "core.k8sgpt.ai",
 		Version:  "v1alpha1",
-		Resource: "results.core.k8sgpt.ai",
+		Resource: "results",
 	}
 
-	resultList, err := dynamic.Resource(resourceId).Namespace(namespace).
-		Get(ctx, name, metav1.GetOptions{})
-
+	resultList, err := dynamic.Resource(resourceId).Namespace(namespace).List(context.TODO(), metav1.ListOptions{})
 	if err != nil {
-		// Handle error
-		log.Fatalf("Failed to get ResultList: %v", err)
+		logger.Error("Failed to list Result object:", zap.Error(err))
 	}
 
-	// Extract the items from the ResultList
-	items, found := resultList.Object["items"]
-	if !found {
-		// Handle if items field not found
-		log.Fatalf("Items not found in ResultList")
-	}
+	for _, item := range resultList.Items {
+		name := item.GetName()
+		resultData := item.Object
 
-	// Type assertion to []interface{} since items is an array
-	resultListItems, ok := items.([]interface{})
-	if !ok {
-		// Handle if items is not of the expected type
-		log.Fatalf("Failed to convert items to []interface{}")
-	}
-
-	// Iterate over the resultListItems
-	for _, item := range resultListItems {
-		// Type assertion to map[string]interface{} since each item is a map
-		resultItem, ok := item.(map[string]interface{})
+		details, ok := resultData["spec"].(map[string]interface{})["details"].(string)
 		if !ok {
-			// Handle if item is not of the expected type
-			log.Fatalf("Failed to convert item to map[string]interface{}")
+			logger.Info("Failed to extract details field from Result object")
 		}
 
-		// Extract the desired fields from the resultItem map
-		// Adjust the field names as per your CRD schema
-		name := resultItem["metadata"].(map[string]interface{})["name"].(string)
-		details := resultItem["spec"].(map[string]interface{})["details"].(string)
+		errorArray, ok := resultData["spec"].(map[string]interface{})["error"].([]interface{})
+		if !ok || len(errorArray) == 0 {
+			logger.Info("Failed to extract error field from Result object")
+		}
 
-		// Print the extracted values
-		fmt.Printf("Name: %s, Details: %s\n", name, details)
+		errorText, ok := errorArray[0].(map[string]interface{})["text"].(string)
+		if !ok {
+			logger.Info("Failed to extract text field from Result object")
+		}
+
+		fmt.Println("*****")
+		fmt.Println("Name:", name)
+		fmt.Println("Error Text:", errorText)
+		fmt.Println("Details:", details)
+		fmt.Println("*****")
 	}
-
-	//x := resultList.DeepCopyInto().Object
-	if err != nil {
-		fmt.Errorf("Error getting Provider: %s", err)
-	}
-
-	//clientset := kubernetes.NewForConfigOrDie(config)
-	// secret, err := clientset.CoreV1().Secrets(namespace).
-	// 	Get(ctx, name, metav1.GetOptions{})
-	// GroupVersion := schema.GroupVersion{Group: "core.k8sgpt.ai", Version: "v1alpha1"}
-
-	// SchemeBuilder is used to add go types to the GroupVersionKind scheme
-	// SchemeBuilder := &scheme.Builder{GroupVersion: GroupVersion}
-	// SchemeBuilder.Register(&Result{}, &ResultList{})
-	// resultList := &corev1.ResultList{}
-	// listOptions := metav1.ListOptions{}
-
-	// err = clientset.RESTClient().
-	// 	Get().
-	// 	Namespace(namespace).
-	// 	Resource("results").
-	// 	VersionedParams(&listOptions, metav1.ParameterCodec).
-	// 	Do(context.TODO()).
-	// 	Into(resultList)
-	// if err != nil {
-	// 	log.Fatalf("Failed to get ResultList: %v", err)
-	// }
-
-	// // Print the retrieved custom resources
-	// for _, result := range resultList.Items {
-	// 	fmt.Printf("Name: %s, Details: %s\n", result.Name, result.Spec.Details)
-	// }
-
-	return "hi"
 }
